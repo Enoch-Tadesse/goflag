45f99efff5ee813fa9b70452153a780538a45a57
Enoch-Tadesse <henitadesse97@gmail.com>
2025-07-07 11:10:36 +0300

    created get all segments and get segments by name http handler function

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ toolchain go1.23.10
 require (
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
 	github.com/pressly/goose/v3 v3.24.3
 )
---
+++ internal/handlers/segments/getSegments.go
@@ -0,0 +1,174 @@
+package segments
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	conn "github.com/Enoch-Tadesse/goflag/db/connection"
+	"github.com/Enoch-Tadesse/goflag/db/models"
+	"github.com/gorilla/mux"
+)
+
+func GetAllSegments(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// get the segments first
+	rows, err := conn.DB.QueryContext(ctx, `
+		SELECT id, name, created_at
+		FROM segments
+	`, nil)
+
+	if err != nil {
+		log.Printf("GetAllSegments: Failed to fetch all segments. %v", err)
+		http.Error(w, "Failed to fetch segments", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	type Segment struct {
+		ID        string    `json:"id"`
+		Name      string    `json:"name"`
+		CreatedAt time.Time `json:"created_at"`
+	}
+
+	var segments []Segment
+
+	// iterate over all segments and collect
+	for rows.Next() {
+		var segment Segment
+		if err := rows.Scan(&segment.ID, &segment.Name, &segment.CreatedAt); err != nil {
+			log.Printf("GetAllSegments: Error scanning segment row: %v", err)
+			continue
+		}
+		segments = append(segments, segment)
+	}
+
+	if len(segments) == 0 {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": "No segments currently"})
+		return
+	}
+
+	type Rule struct {
+		ID        string `json:"id"`
+		Attribute string `json:"attribute"`
+		Operator  string `json:"operator"`
+		Value     string `json:"value"`
+		CreatedAt string `json:"created_at"`
+	}
+
+	type Result struct {
+		Segment Segment `json:"segment"`
+		Rules   []Rule  `json:"rules"`
+	}
+
+	var results struct {
+		AllSegments []Result `json:"all_segments"`
+	}
+
+	for _, seg := range segments {
+		result := Result{
+			Segment: seg,
+			Rules:   []Rule{},
+		}
+		id := seg.ID
+		// get all segment rules specific to a single segment
+		innerRows, err := conn.DB.QueryContext(ctx, `
+			SELECT id, attribute, operator, value, created_at
+			FROM segment_rules
+			WHERE seg_id = ?
+		`, id)
+		if err != nil {
+			log.Printf("GetAllSegments: Failed to fetch rules for segment id: %s, name: %s: %v", seg.ID, seg.Name, err)
+			http.Error(w, fmt.Sprintf("Failed to fetch rules for %s segment", seg.Name), http.StatusInternalServerError)
+			return
+		}
+		// Immediate Invoked Function Expression for defer innerRows resource wastage
+		func() {
+			defer innerRows.Close()
+			result.Segment = seg
+
+			for innerRows.Next() {
+				var rule Rule
+				if err := innerRows.Scan(&rule.ID, &rule.Attribute, &rule.Operator, &rule.Value, &rule.CreatedAt); err != nil {
+					log.Printf("GetAllSegments: Failed to scan innerRow: %v", err)
+					continue
+				}
+				result.Rules = append(result.Rules, rule)
+			}
+		}()
+		results.AllSegments = append(results.AllSegments, result)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
+}
+
+func GetSegmentByName(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	// get the segment by name
+	row := conn.DB.QueryRowContext(ctx, `
+		SELECT id, name, created_at
+		FROM segments
+		WHERE name = ?
+		LIMIT 1
+	`, name)
+
+	var segment models.Segment
+
+	err := row.Scan(&segment.ID, &segment.Name, &segment.CreatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, fmt.Sprintf("Segment with name %s does not exist", name), http.StatusBadRequest)
+			return
+		}
+		log.Printf("GetSegmentByName: Failed to check if segment %s exists: %v", name, row.Err())
+		http.Error(w, "Failed to check if segment exist", http.StatusInternalServerError)
+		return
+	}
+
+	// get the associalted segment rules
+	row = conn.DB.QueryRowContext(ctx, `
+		SELECT id, attribute, operator, value, created_at
+		FROM segment_rules
+		WHERE id = ?
+	`, segment.ID)
+
+	var rule models.SegmentRule
+	var rules []models.SegmentRule
+
+	// scan all the segment rules
+	for {
+		if err := row.Scan(&rule.ID, &rule.Attribute, &rule.Operator, &rule.Value, &rule.CreatedAt); err != nil {
+			if err == sql.ErrNoRows {
+				break
+			}
+			log.Printf("GetSegmentByName: Failed to scan result rows: %v", err)
+			http.Error(w, "Failed to fetch segment rules", http.StatusInternalServerError)
+			return
+		}
+		rules = append(rules, rule)
+	}
+
+	var response struct {
+		Segment models.Segment       `json:"segment"`
+		Rules   []models.SegmentRule `json:"rules"`
+	}
+
+	response.Segment = segment
+	response.Rules = rules
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+}
