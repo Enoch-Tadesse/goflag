efebb8ab60c7ffa3f69f9d64280666d48d69d38e
Enoch-Tadesse <henitadesse97@gmail.com>
2025-07-23 06:27:04 +0300

    added a utility function to check zero fields of a struct

--- internal/handlers/segments/util.go
+++ internal/handlers/segments/util.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"reflect"
 )
 
 func rollbackWithError(w http.ResponseWriter, tx *sql.Tx, logMsg string, errMsg string, err error) {
@@ -12,4 +13,45 @@ func rollbackWithError(w http.ResponseWriter, tx *sql.Tx, logMsg string, errMsg 
 		log.Printf("CreateSegment: Rollback also failed: %v", rbErr)
 	}
 	http.Error(w, errMsg, http.StatusInternalServerError)
+}
+
+func zeroFields(data any, exclude ...string) []string {
+	v := reflect.ValueOf(data)
+	t := reflect.TypeOf(data)
+
+	// dereference if data is a pointer
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+		t = t.Elem()
+	}
+
+	excluded := make(map[string]bool)
+	for _, name := range exclude {
+		excluded[name] = true
+	}
+
+	var zeroFieldNames []string
+
+	for i := range v.NumField() {
+		field := v.Field(i)
+		fieldType := t.Field(i)
+
+		// skip unexported fields
+		if !field.CanInterface() {
+			continue
+		}
+
+		// skip excluded fields
+		if excluded[fieldType.Name] {
+			continue
+		}
+
+		// built-in checker for zero values
+		if field.IsZero() {
+			zeroFieldNames = append(zeroFieldNames, fieldType.Name)
+		}
+
+	}
+	return zeroFieldNames
+
 }
